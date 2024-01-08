6e019d40b31188f5b39b5f1c2fe68f572db9753b
adobrotvorskiy <143310339+adobrotvorskiy@users.noreply.github.com>
2024-01-08 13:45:24 +0300

    Update device.py

--- pymelcloud/device.py
+++ pymelcloud/device.py
@@ -94,7 +94,7 @@ class Device(ABC):
             and c.get("BuildingID") == self.building_id
         )
         self._state = await self._client.fetch_device_state(self)
-        self._energy_report = await self._client.fetch_energy_report(self)
+#        self._energy_report = await self._client.fetch_energy_report(self)
 
         if self._device_units is None and self.access_level != ACCESS_LEVEL.get(
             "GUEST"
